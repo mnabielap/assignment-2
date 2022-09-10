6aa3726b24c8d58d8c23008227d6a5b007c380d9
mnabielap <mnabielap@gmail.com>
2022-09-10 18:48:43 +0700

    Melengkapi katalog app

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
 
 # TODO: Create your views here.
+def show_katalog(request):
+    daftar_item_katalog = CatalogItem.objects.all()
+    context = {
+        'list_item': daftar_item_katalog,
+        'nama': 'Muhammad Nabiel Andityo Purnomo',
+        'npm': '2106750465',
+    }
+    return render(request, 'katalog.html', context)
